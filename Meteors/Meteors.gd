2365d8ffc0eed6a509f5288258eb35205fe9fc08
Alex <58958500+SpaceWK@users.noreply.github.com>
2022-05-17 20:01:06 +0300

    Folders changes

---
+++ Meteors/Meteors.gd
@@ -0,0 +1,17 @@
+extends KinematicBody
+
+
+export var speed = 0.1
+
+var velocity = Vector3.ZERO
+var rotation_speed = 0.1
+
+func _process(delta):
+	var direction = Vector3.ZERO
+
+	direction.x += 1
+	
+	velocity.x = direction.x * speed
+	# Moving the character
+	velocity = move_and_slide(velocity, Vector3.UP)
+	self.rotate_y(self.rotation_speed * delta)
